9c14453d9d26d27c6a8d7c8e5e08293bbf6d118a
Kimiaki Kuno <knokmki612@gmail.com>
2020-12-25 00:55:32 +0900

    Use notion post in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,62 @@
-import Link from 'next/link'
-import Header from '../components/header'
-import ExtLink from '../components/ext-link'
-import Features from '../components/features'
-import GitHub from '../components/svgs/github'
-import sharedStyles from '../styles/shared.module.css'
+import fetch from 'node-fetch'
+import BlogPost from '../components/blog-post'
+import getPageData from '../lib/notion/getPageData'
+import getBlogIndex from '../lib/notion/getBlogIndex'
+import getNotionUsers from '../lib/notion/getNotionUsers'
 
-export default () => (
-  <>
-    <Header titlePre="Home" />
-    <div className={sharedStyles.layout}>
-      <img
-        src="/logo.png"
-        height="85"
-        width="250"
-        alt="Vercel + Notion"
-      />
-      <h1>Barloon</h1>
-      <h2>
-        Blazing Fast Notion Blog with Next.js'{' '}
-        <ExtLink
-          href="https://github.com/vercel/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      </h2>
+// Get the data for each blog post
+export async function getStaticProps({ preview }) {
+  // load the postsTable so that we can get the page's ID
+  const postsTable = await getBlogIndex({ type: 'page' })
+  const post = postsTable['top']
 
-      <Features />
+  // if we can't find the post or if it is unpublished and
+  // viewed without preview mode then we just redirect to /
+  if (!post || (post.Published !== 'Yes' && !preview)) {
+    console.log(`Failed to find post for slug: top`)
+    return {
+      props: {
+        redirect: '/',
+        preview: false,
+      },
+      unstable_revalidate: 5,
+    }
+  }
+  const postData = await getPageData(post.id)
+  post.content = postData.blocks
 
-      <div className="explanation">
-        <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>.
-          It leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
-        </p>
+  for (let i = 0; i < postData.blocks.length; i++) {
+    const { value } = postData.blocks[i]
+    const { type, properties } = value
+    if (type == 'tweet') {
+      const src = properties.source[0][0]
+      // parse id from https://twitter.com/_ijjk/status/TWEET_ID format
+      const tweetId = src.split('/')[5].split('?')[0]
+      if (!tweetId) continue
 
-        <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          page's id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new page's id. To get your token from Notion, login
-          and look for a cookie under www.notion.so with the name `token_v2`.
-          After finding your token and your blog's page id you should be good to
-          go!
-        </p>
-      </div>
-    </div>
-  </>
-)
+      try {
+        const res = await fetch(
+          `https://api.twitter.com/1/statuses/oembed.json?id=${tweetId}`
+        )
+        const json = await res.json()
+        properties.html = json.html.split('<script')[0]
+        post.hasTweet = true
+      } catch (_) {
+        console.log(`Failed to get tweet embed for ${src}`)
+      }
+    }
+  }
+
+  const { users } = await getNotionUsers(post.Authors || [])
+  post.Authors = Object.keys(users).map(id => users[id].full_name)
+
+  return {
+    props: {
+      post,
+      preview: preview || false,
+    },
+    unstable_revalidate: 10,
+  }
+}
+
+export default BlogPost
