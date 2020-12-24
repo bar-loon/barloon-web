2d10a2b2ebb60bf850398626d7af3e89d8762e22
Kimiaki Kuno <knokmki612@gmail.com>
2020-12-24 09:52:25 +0900

    Add multiple page table

--- src/lib/notion/getBlogIndex.ts
+++ src/lib/notion/getBlogIndex.ts
@@ -4,12 +4,27 @@ import createTable from './createTable'
 import getTableData from './getTableData'
 import { getPostPreview } from './getPostPreview'
 import { readFile, writeFile } from '../fs-helpers'
-import { BLOG_INDEX_ID, BLOG_INDEX_CACHE } from './server-constants'
+import {
+  BLOG_INDEX_ID,
+  OTHER_INDEX_ID,
+  BLOG_INDEX_CACHE,
+} from './server-constants'
 
-export default async function getBlogIndex(previews = true) {
+const types = {
+  blog: BLOG_INDEX_ID,
+  other: OTHER_INDEX_ID,
+} as const
+
+export default async function getBlogIndex({
+  type = 'blog',
+  previews = true,
+} = {}) {
   let postsTable: any = null
+  const indexId = types[type]
   const useCache = process.env.USE_CACHE === 'true'
-  const cacheFile = `${BLOG_INDEX_CACHE}${previews ? '_previews' : ''}`
+  const cacheFile = `${BLOG_INDEX_CACHE}${indexId}${
+    previews ? '_previews' : ''
+  }`
 
   if (useCache) {
     try {
@@ -22,7 +37,7 @@ export default async function getBlogIndex(previews = true) {
   if (!postsTable) {
     try {
       const data = await rpc('loadPageChunk', {
-        pageId: BLOG_INDEX_ID,
+        pageId: indexId,
         limit: 999, // TODO: figure out Notion's way of handling pagination
         cursor: { stack: [] },
         chunkNumber: 0,
@@ -44,7 +59,7 @@ export default async function getBlogIndex(previews = true) {
         console.log(`Successfully created table in Notion`)
       } catch (err) {
         console.error(
-          `Auto creating table failed, make sure you created a blank page and site the id with BLOG_INDEX_ID in your environment`,
+          `Auto creating table failed, make sure you created a blank page and site the id with BLOG_INDEX_ID or OTHER_INDEX_ID in your environment`,
           err
         )
       }
