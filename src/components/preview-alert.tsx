ddbb6329126056ec0762b5a7eb4f421e5ad573ff
Kimiaki Kuno <knokmki612@gmail.com>
2021-01-09 21:23:30 +0900

    refactor: use common class name

--- src/components/preview-alert.tsx
+++ src/components/preview-alert.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import styles from '../styles/preview-alert.module.css'
 
 export default ({ slug = '' }) => (
-  <div className={styles.previewAlertContainer}>
-    <div className={styles.previewAlert}>
+  <div className={styles.root}>
+    <div className={styles.inner}>
       <b>Note:</b>
       {` `}Viewing in preview mode{' '}
       <Link href={`/api/clear-preview${slug && `?slug=${slug}`}`}>
